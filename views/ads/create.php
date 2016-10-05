1a7901bdada0c6d3dc9327d1c34a011f279cafe9
Justin Reich <justinreich.dev@gmail.com>
2016-10-04 21:16:50 -0500

    Markup for ad creation (create.php)

--- views/ads/create.php
+++ views/ads/create.php
@@ -17,28 +17,45 @@
 </head>
 
 <style type="text/css">
+   /* * {
+        border: 1px lime solid;
+    }*/
     body {
         background-color: LightSteelBlue;
-        border: 1px lime solid;
     }
-    
-
+    .custom-ad-img {
+        width: 100%;
+    }
+    .custom-ad-img a {
+        margin: 0;
+    }
+    .custom-h3 {
+        text-align: center;
+    }
+    .custom-anchor {
+        margin: 0;
+    }
+    .custom-ad-edit-main {
+        text-align: center;
+    }
 </style>
-<main>
-    
+
+<main class="container-fuid custom-ad-edit-main">
+    <h2>Create Item</h2>
+    <!-- <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2"> -->
+    <div class="col-xs-offset-2 col-xs-8 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4">
+    <!-- <div> -->
+        <form class="form-group">
+            <label for="name">Name</label><br>
+            <input id="name" type="text" name="name" class="form-control" value=""><br>
+            <label>Price</label><br>
+            <input id="price" type="text" name="price" class="form-control" value=""><br>
+            <label>Description</label><br>
+            <textarea id="description" type="text" name="description" class="form-control" value="" rows="5"></textarea><br>
+            <button type="submit" class="btn btn-default">Submit</button><br>
+        </form>
+    </div>
 </main>
-   
-    <!-- <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2">
-            <div class="custom-ad">
-                <a href="#" id="#"> 
-                    <img src="http://proflikesubstance.scientopia.org/wp-content/uploads/sites/23/2011/02/Box.jpg" class="custom-ad-img">
-                    <h5>Placeholder Title</h5>
-                    <p>Placeholder description</p>
-                </a>
-            </div>
-        </div>
-    </div> -->
 
 <!-- jQuery Version 1.11.1 -->
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>

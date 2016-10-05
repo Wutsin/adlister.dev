2085376345fc7ae9ce627c982ed44530bcab07ed
Justin Reich <justinreich.dev@gmail.com>
2016-10-04 20:09:47 -0500

    Fixed incorrect markup for create.php and added correct markup to index.php

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,1 +1,48 @@
+<!-- adlister.dev/create -->
 <!--Page for creating new advertisement listings-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Ad Create Draft</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="===PATH HERE===">
+</head>
+
+<style type="text/css">
+    body {
+        background-color: LightSteelBlue;
+        border: 1px lime solid;
+    }
+    
+
+</style>
+<main>
+    
+</main>
+   
+    <!-- <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2">
+            <div class="custom-ad">
+                <a href="#" id="#"> 
+                    <img src="http://proflikesubstance.scientopia.org/wp-content/uploads/sites/23/2011/02/Box.jpg" class="custom-ad-img">
+                    <h5>Placeholder Title</h5>
+                    <p>Placeholder description</p>
+                </a>
+            </div>
+        </div>
+    </div> -->
+
+<!-- jQuery Version 1.11.1 -->
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+

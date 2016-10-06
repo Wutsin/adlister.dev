8ce9a0342a7bb18686ac8a1f72988eda294131dc
Wyatt Barnes <wyattb413@gmail.com>
2016-10-06 14:32:53 -0500

    Added Item class

---
+++ models/Items.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/Model.php';
+
+class Item extends Model {
+
+  protected static $table = 'items';
+
+
+
+}
+
+//display all the add
+//search for specific by id
+//be able to search through all the collums for keyword

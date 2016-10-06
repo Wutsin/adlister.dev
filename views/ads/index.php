74b129ea6938206ecf09e81450cff8a90ca5181f
Wyatt Barnes <wyattb413@gmail.com>
2016-10-06 16:43:28 -0500

    Changes made to Populate Ad info

--- views/ads/index.php
+++ views/ads/index.php
@@ -1,145 +1,24 @@
-<!-- adlister.dev/index -->
-<!--Page for an index of advertisements-->
-
-
 <main>
     <h2>Items for Sale</h2>
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+
+            <?php foreach($items->attributes as $item) : ?>
+            <div class="col-xs-12x   col-sm-6 col-md-6 col-lg-4">
                 <div class="thumbnail">
                     <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
                         <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                            <img src=<?= $item['image_url']; ?> alt="Image File Path Bad" class="custom-adINDEX-ad-img">
+                            <h4 class="custom-adINDEX-h3"><?= $item['headline']; ?></h4>
+                            <p><?= '$' . $item['price']; ?></p>
+                            <p><?= $item['state']; ?></p>
+                            <p><?= $item['county']; ?></p>
                         </div>
                     </a>
                 </div>
             </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-                <div class="thumbnail">
-                    <a href="#" class="thumbnail custom-adINDEX-anchor"> <!-- this link will go to the item page -->
-                        <div class="caption">
-                            <img src="/testbox.png" alt="Image File Path Bad" class="custom-adINDEX-ad-img">
-                            <h4 class="custom-adINDEX-h3">Title</h4>
-                            <p>Placeholder Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            <?php endforeach; ?>
+
         </div>
-    </div>    
+    </div>
 </main>
-
-

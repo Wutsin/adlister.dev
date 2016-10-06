f07cbc0241b73d08ac07c9765444bd53855829a4
Justin Reich <justinreich.dev@gmail.com>
2016-10-05 19:11:00 -0500

    Took out all js script tags for ad views

--- views/ads/edit.php
+++ views/ads/edit.php
@@ -126,25 +126,6 @@
     </div>
 </main>
 
-<script src="http://code.jquery.com/jquery-1.12.2.min.js"></script>
-<script type="text/javascript" src="/js/image-load.js"></script>
-<script type="text/javascript" src="/js/drop_down.js"></script>
-<script>
-    $('.imageupload').imageupload();
-</script>
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 
 
--- views/partials/common_js.php
+++ views/partials/common_js.php
@@ -1,7 +1,17 @@
 <!-- jQuery Version 1.11.1 -->
-  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-<!-- jQuery Version 1.11.1 -->
+<!-- <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> -->
+<script src="http://code.jquery.com/jquery-1.12.2.min.js"></script>
+
 
 <!-- Bootstrap Core JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<!-- Bootstrap Core JavaScript -->
+
+<!-- For image loader -->
+<script type="text/javascript" src="/js/image-load.js"></script>
+
+<!-- For State/County -->
+<script type="text/javascript" src="/js/drop_down.js"></script>
+
+<!-- For image uploader *** MUST STAY UNDER image-load AND drop_down.js -->
+
+<script src="/js/ad-create.js"></script>

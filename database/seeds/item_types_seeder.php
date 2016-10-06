ff18c816b530ffc114c91914f195e8444307c0c0
Justin Reich <justinreich.dev@gmail.com>
2016-10-06 14:54:42 -0500

    Seeder commit from wrong branch. item_types_seeder created

--- database/seeds/item_types_seeder.php
+++ database/seeds/item_types_seeder.php
@@ -3,4 +3,13 @@
 require_once __DIR__ . '/../../models/Item_Type.php';
 
 $item_type = new Item_Type();
+$item_type->name = 'Bicycle';
+$item_type->save();
 
+$item_type = new Item_Type();
+$item_type->name = 'Unicycle';
+$item_type->save();
+
+$item_type = new Item_Type();
+$item_type->name = 'Skateboard';
+$item_type->save();

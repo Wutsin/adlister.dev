9c92a7097973f2dbcef5ac666c99fd19d0beddf0
Wyatt Barnes <wyattb413@gmail.com>
2016-10-07 15:30:26 -0500

    Made all ads have the same size

--- views/ads/index.php
+++ views/ads/index.php
@@ -5,9 +5,9 @@
 
             <?php foreach($items->attributes as $item) : ?>
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-                <div class="thumbnail">
+                <div class="thumbnail customThumbNail">
                     <a href="/show?itemId=<?= $item['id']; ?>" class="thumbnail custom-adINDEX-anchor">
-                        <div class="caption">
+                        <div class="caption customCaption">
                             <img src=<?= $item['image_url']; ?> alt="Image File Path Bad" class="custom-adINDEX-ad-img">
                             <h4 class="custom-adINDEX-h3"><?= $item['headline']; ?></h4>
                             <p><?= '$' . $item['price']; ?></p>

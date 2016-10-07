a3be4f1eab630cb77d07de2b2d24b5e9033ba619
Wyatt Barnes <wyattb413@gmail.com>
2016-10-07 13:22:04 -0500

    Changed URL Request that caused a bug with populating input fields on edit page

--- views/ads/show.php
+++ views/ads/show.php
@@ -14,7 +14,7 @@
                         </p>
                     </div>
                 </div>
-                <a href="/edit?id=<?= $items->attributes['id']; ?>"><button class="btn btn-primary custom-adSHOW-show-btn center-block">Edit Ad</button></a>
+                <a href="/edit?itemId=<?= $items->attributes['id']; ?>"><button class="btn btn-primary custom-adSHOW-show-btn center-block">Edit Ad</button></a>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                 <div class="custom-adSHOW-center">

521bc14c36367b17f5f3bd78da83e60673fbf80f
Mittsy Tidwell <mtidwell506@gmail.com>
2016-10-08 17:08:01 -0500

    trying to figure out delete and reverted back

--- views/ads/edit.php
+++ views/ads/edit.php
@@ -127,7 +127,7 @@
             </div>
 
             <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 text-center">
-              <a href="" class="btn btn-danger loginButtons">Delete Ad</a>
+              <a href="#" class="btn btn-danger loginButtons">Delete Ad</a>
             </div>
         </form>
     </div>

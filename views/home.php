f99a585907980b9aefe3c8fd8ebf048319ddd8c3
Wyatt Barnes <wyattb413@gmail.com>
2016-10-08 11:55:21 -0500

    Added functionality to categories on home page

--- views/home.php
+++ views/home.php
@@ -22,7 +22,7 @@
                 <!-- <div class="row"> -->
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                         <div class="thumbnail">
-                            <a href="#" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
+                            <a href="/ads?search=skateboard" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
                                 <div class="caption">
                                     <img src="/img/hulk_skateboard.png" alt="Image File Path Bad" class="featuredImg">
                                     <h4 class="custom-h3">Skateboards</h4>
@@ -33,7 +33,7 @@
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                         <div class="thumbnail">
-                            <a href="#" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
+                            <a href="/ads?search=bicycle" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
                                 <div class="caption">
                                     <img src="/img/mars_attacks_bike.jpg" alt="Image File Path Bad" class="featuredImg">
                                     <h4 class="custom-h3">Bicycles</h4>
@@ -44,7 +44,7 @@
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                         <div class="thumbnail">
-                            <a href="#" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
+                            <a href="/ads?search=unicycle" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
                                 <div class="caption">
                                     <img src="/img/gizmoduck_uni.jpg" alt="Image File Path Bad" class="featuredImg">
                                     <h4 class="custom-h3">Unicycles</h4>

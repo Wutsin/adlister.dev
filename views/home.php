226e9b58c6f844926588a505cd09d9e132ef1a1f
Mittsy Tidwell <mtidwell506@gmail.com>
2016-10-08 19:43:28 -0500

    styling the landing page

--- views/home.php
+++ views/home.php
@@ -1,61 +1,46 @@
 <div class="container">
+    <div class="row">
 
-    <section id="welcome">
-
-        <div class="row">
-
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-                <h1 class="text-center">Welcome To Wutsin Wheels</h1>
-
-            </div>
-
+        <!-- Header -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><br>
+            <h1 class="text-center">Welcome To Wutsin Wheels</h1>
         </div>
-
-    </section>
+    </div>
 
     <section id="features">
-        <div class="row">
+        <div class="row"><br><br><br>
             <h3 class="section-title featuredItemsHeader">Categories</h3>
-            <hr>
-            <div class="container">
-                <!-- <div class="row"> -->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                        <div class="thumbnail">
-                            <a href="/ads?search=skateboard" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
-                                <div class="caption">
-                                    <img src="/img/hulk_skateboard.png" alt="Image File Path Bad" class="featuredImg">
-                                    <h4 class="custom-h3">Skateboards</h4>
-                                    <p>View Skateboard Ads</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                        <div class="thumbnail">
-                            <a href="/ads?search=bicycle" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
-                                <div class="caption">
-                                    <img src="/img/mars_attacks_bike.jpg" alt="Image File Path Bad" class="featuredImg">
-                                    <h4 class="custom-h3">Bicycles</h4>
-                                    <p>View Bicycle Ads</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                        <div class="thumbnail">
-                            <a href="/ads?search=unicycle" class="thumbnail custom-anchor"> <!-- this link will go to the item page -->
-                                <div class="caption">
-                                    <img src="/img/gizmoduck_uni.jpg" alt="Image File Path Bad" class="featuredImg">
-                                    <h4 class="custom-h3">Unicycles</h4>
-                                    <p>View Unicycle Ads</p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                <!-- </div> -->
-            </div>
-        </div>
-    </section>
 
-</div>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <!-- skateboard -->
+                <a href="/ads?search=skateboard" class="thumbnail custom-anchor"> 
+                    <!-- image and wording under image -->
+                    <div class="caption">
+                        <img src="/img/skateboard_landing.png" alt="Image File Path Bad" class="featuredImg">
+                        <h4 class="custom-h3">Skateboards</h4>
+                    </div>
+                </a>
+            </div> <!-- closes skateboards --> 
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <!-- Bicycles -->
+                <a href="/ads?search=bicycle" class="thumbnail custom-anchor"> 
+                    <div class="caption">
+                        <img src="/img/bike_landing.png" alt="Image File Path Bad" class="featuredImg">
+                        <h4 class="custom-h3">Bicycles</h4>
+                    </div>
+                </a>
+            </div> <!-- closes bicycles -->
+
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                <!-- Unis -->
+                <a href="/ads?search=unicycle" class="thumbnail custom-anchor">
+                    <div class="caption">
+                        <img src="/img/uni_landing.png" alt="Image File Path Bad" class="featuredImg">
+                        <h4 class="custom-h3">Unicycles</h4>
+                    </div> 
+                </a>
+            </div><!-- closes Unis -->
+        </div> <!-- closes row -->
+    </section>
+</div> <!-- closes container -->
